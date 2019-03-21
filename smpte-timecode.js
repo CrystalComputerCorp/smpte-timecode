6c6d9f565ea692e40b4fe3dc8c348c9d18acfc18
nkurdybakha <48804445+nkurdybakha@users.noreply.github.com>
2019-03-21 13:56:25 -0400

    Fix for issues #4: Date Conversion Issue

--- smpte-timecode.js
+++ smpte-timecode.js
@@ -1,14 +1,14 @@
 // This should work both in node and in the browsers, so that's what this wrapper is about
 ;(function(root, undefined) {
 
-    
+
     /**
      * Timecode object constructor
      * @param {number|String|Date|Object} timeCode Frame count as number, "HH:MM:SS(:|;|.)FF", Date(), or object.
      * @param {number} [frameRate=29.97] Frame rate
      * @param {boolean} [dropFrame=true] Whether the timecode is drop-frame or not
      * @constructor
-     * @returns {Timecode}
+     * @returns {Timecode} timecode
      */
     var Timecode = function ( timeCode, frameRate, dropFrame ) {
 
@@ -16,78 +16,79 @@
         if (!(this instanceof Timecode)) return new Timecode( timeCode, frameRate, dropFrame);
 
         // Get frame rate
-        if (typeof frameRate == 'undefined') this.frameRate = 29.97;
-        else if (typeof frameRate == 'number' && frameRate>0) this.frameRate = frameRate;
+        if (typeof frameRate === 'undefined') this.frameRate = 29.97;
+        else if (typeof frameRate === 'number' && frameRate>0) this.frameRate = frameRate;
         else throw new Error('Number expected as framerate');
-        if (this.frameRate!=23.976 && this.frameRate!=24 && this.frameRate!=25 && this.frameRate!=29.97 && this.frameRate!=30 &&
-            this.frameRate!=50 && this.frameRate!=59.94 && this.frameRate!=60
+        if (this.frameRate!==23.976 && this.frameRate!==24 && this.frameRate!==25 && this.frameRate!==29.97 && this.frameRate!==30 &&
+            this.frameRate!==50 && this.frameRate!==59.94 && this.frameRate!==60
         ) throw new Error('Unsupported framerate');
-        
+
         // If we are passed dropFrame, we need to use it
         if (typeof dropFrame === 'boolean') this.dropFrame = dropFrame;
-        else this.dropFrame = (this.frameRate==29.97 || this.frameRate==59.94); // by default, assume DF for 29.97 and 59.94, NDF otherwise
+        else this.dropFrame = (this.frameRate===29.97 || this.frameRate===59.94); // by default, assume DF for 29.97 and 59.94, NDF otherwise
 
         // Now either get the frame count, string or datetime        
-        if (typeof timeCode == 'number') {
-            this.frameCount = Math.floor(timeCode);
+        if (typeof timeCode === 'number') {
+            this.frameCount = Math.round(timeCode);
+            this._frameCountToTimeCode();
         }
-        else if (typeof timeCode == 'string') {
-            
+        else if (typeof timeCode === 'string') {
             // pick it apart
             var parts = timeCode.match('^([012]\\d):(\\d\\d):(\\d\\d)(:|;|\\.)(\\d\\d)$');
             if (!parts) throw new Error("Timecode string expected as HH:MM:SS:FF or HH:MM:SS;FF");
             this.hours = parseInt(parts[1]);
             this.minutes = parseInt(parts[2]);
             this.seconds = parseInt(parts[3]);
-            this.dropFrame = parts[4]!=':';
+            // do not override input parameters
+            if (typeof dropFrame !== 'boolean') {
+                this.dropFrame = parts[4]!==':';
+            }
             this.frames = parseInt(parts[5]);
-
-            // make sure the numbers make sense
-            if ( this.hours>23 || this.minutes>59 || this.seconds>59 || 
-                 this.frames>=this.frameRate ||
-                 (this.dropFrame && this.seconds==0 && this.minutes%10 && this.frames<2*(this.frameRate/29.97) )
-            ) throw new Error("Invalid timecode")
+            this._timeCodeToFrameCount();
         }
-        else if (typeof timeCode == 'object' && timeCode instanceof Date) {
-            var midnight = new Date( timeCode.getFullYear(), timeCode.getMonth(), timeCode.getDate(),0,0,0 );
+        else if (typeof timeCode === 'object' && timeCode instanceof Date) {
+            var midnight = new Date(timeCode.getFullYear(), timeCode.getMonth(), timeCode.getDate(),0,0,0);
             var midnight_tz = midnight.getTimezoneOffset() * 60 * 1000;
             var timecode_tz = timeCode.getTimezoneOffset() * 60 * 1000;
-            this.frameCount = Math.floor(((timeCode-midnight + (midnight_tz - timecode_tz))*this.frameRate)/1000);
+            this.frameCount = Math.round(((timeCode-midnight + (midnight_tz - timecode_tz))*this.frameRate)/1000);
+            this._frameCountToTimeCode();
         }
         else if (typeof timeCode === 'object' && timeCode.hours >= 0) {
             this.hours = timeCode.hours;
             this.minutes = timeCode.minutes;
             this.seconds = timeCode.seconds;
             this.frames = timeCode.frames;
-
-            // make sure the numbers make sense
-            if ( this.hours>23 || this.minutes>59 || this.seconds>59 || 
-                 this.frames>=this.frameRate ||
-                 (this.dropFrame && this.seconds==0 && this.minutes%10 && this.frames<2*(this.frameRate/29.97) )
-            )
-            {
-               throw new Error("Invalid timecode: " + JSON.stringify(timeCode));
-            }
+            this._timeCodeToFrameCount();
         }
-        else if (typeof timeCode == 'undefined') {
+        else if (typeof timeCode === 'undefined') {
             this.frameCount = 0;
-        } 
+        }
         else {
             throw new Error('Timecode() constructor expects a number, timecode string, or Date()');
         }
 
+        this._validate(timeCode);
+
+        return this;
+    };
+
+    /**
+     * Validates timecode
+     * @private
+     * @param {number|String|Date|Object} timeCode for the reference
+     */
+    Timecode.prototype._validate = function (timeCode) {
+
         // Make sure dropFrame is only for 29.97 & 59.94
-        if (this.dropFrame && this.frameRate!=29.97 && this.frameRate!=59.94) {
+        if (this.dropFrame && this.frameRate!==29.97 && this.frameRate!==59.94) {
             throw new Error('Drop frame is only supported for 29.97 and 59.94 fps');
         }
 
-        // Now, if we have frameCount we need to calculate hours minutes seconds frames, vice versa
-        if (typeof(this.frameCount)!=='number') {
-            this._timeCodeToFrameCount();
+        // make sure the numbers make sense
+        if (this.hours > 23 || this.minutes > 59 || this.seconds > 59 || this.frames >= this.frameRate ||
+            (this.dropFrame && this.seconds === 0 && this.minutes % 10 && this.frames < 2 * (this.frameRate / 29.97))) {
+            throw new Error("Invalid timecode" + JSON.stringify(timeCode));
         }
-        else this._frameCountToTimeCode();
-
-        return this;
     };
 
     /**
@@ -98,11 +99,11 @@
         var fc = this.frameCount;
         // adjust for dropFrame
         if (this.dropFrame) {
-            var df = this.frameRate==29.97 ? 2 : 4; // 59.94 skips 4 frames
+            var df = this.frameRate===29.97 ? 2 : 4; // 59.94 skips 4 frames
             var d = Math.floor(this.frameCount / (17982*df/2));
             var m = this.frameCount % (17982*df/2);
             if (m<df) m=m+df;
-            fc = this.frameCount + 9*df*d + df*(Math.floor((m-df)/(1798*df/2)));
+            fc += 9*df*d + df*Math.floor((m-df)/(1798*df/2));
         }
         var fps = Math.round(this.frameRate);
         this.frames = fc % fps;
@@ -117,22 +118,24 @@
      */
     Timecode.prototype._timeCodeToFrameCount = function() {
         this.frameCount = (this.hours*3600 + this.minutes*60 + this.seconds) * Math.round(this.frameRate) + this.frames;
+        // adjust for dropFrame
         if (this.dropFrame) {
             var totalMinutes = this.hours*60 + this.minutes;
-            var df = this.frameRate == 29.97 ? 2 : 4;
-            this.frameCount = this.frameCount - df * (totalMinutes - Math.floor(totalMinutes/10));
+            var df = this.frameRate === 29.97 ? 2 : 4;
+            this.frameCount -= df * (totalMinutes - Math.floor(totalMinutes/10));    
         }
     };
 
     /**
      * Convert Timecode to String
-     * @returns {string}
+     * @param {String} format output format
+     * @returns {string} timecode
      */
     Timecode.prototype.toString = function TimeCodeToString(format) {
         var frames = this.frames;
         var field = '';
-        if (typeof format == 'string') {
-            if (format == 'field') {
+        if (typeof format === 'string') {
+            if (format === 'field') {
                 if (this.frameRate<=30) field = '.0';
                 else {
                     frames = Math.floor(frames/2);
@@ -158,7 +161,7 @@
     };
 
     /**
-     * Return the frame count when Timecode() object is used as a number
+     * @returns {Number} the frame count when Timecode() object is used as a number
      */
     Timecode.prototype.valueOf = function() {
         return this.frameCount;
@@ -169,27 +172,27 @@
      * @param {number|string|Date|Timecode} t How much to add
      * @param {boolean} [negative=false] Whether we are adding or subtracting
      * @param {Number} [rollOverMaxHours] allow rollovers
-     * @returns Timecode
+     * @returns {Timecode} timecode
      */
-    Timecode.prototype.add = function(t,negative,rollOverMaxHours) {
+    Timecode.prototype.add = function (t, negative, rollOverMaxHours) {
         if (typeof t === 'number') {
-            var newFrameCount = this.frameCount + Math.floor(t) * (negative?-1:1);
+            var newFrameCount = this.frameCount + Math.round(t) * (negative?-1:1);
             if (newFrameCount<0 && rollOverMaxHours > 0) {
-               newFrameCount = (Math.floor(this.frameRate*86400)) + newFrameCount;
-               if (((newFrameCount / this.frameRate) / 3600) > rollOverMaxHours) {
-                   throw new Error('Rollover arithmetic exceeds max permitted');
-               }
+                newFrameCount = (Math.round(this.frameRate*86400)) + newFrameCount;
+                if (((newFrameCount / this.frameRate) / 3600) > rollOverMaxHours) {
+                    throw new Error('Rollover arithmetic exceeds max permitted');
+                }
             }
-           if (newFrameCount<0) {
-              throw new Error("Negative timecodes not supported");
-           }
+            if (newFrameCount<0) {
+                throw new Error("Negative timecodes not supported");
+            }
             this.frameCount = newFrameCount;
-        } 
+        }
         else {
             if (!(t instanceof Timecode)) t = new Timecode(t);
             return this.add(t.frameCount,negative,rollOverMaxHours);
         }
-        this.frameCount = this.frameCount % (Math.floor(this.frameRate*86400)); // wraparound 24h
+        this.frameCount = this.frameCount % (Math.round(this.frameRate*86400)); // wraparound 24h
         this._frameCountToTimeCode();
         return this;
     };
@@ -201,10 +204,10 @@
 
     /**
      * Converts timecode to a Date() object
-     * @returns {Date}
+     * @returns {Date} date
      */
     Timecode.prototype.toDate = function() {
-        var ms = this.frameCount/this.frameRate*1000
+        var ms = this.frameCount/this.frameRate*1000;
         var midnight = new Date();
         midnight.setHours(0);
         midnight.setMinutes(0);
